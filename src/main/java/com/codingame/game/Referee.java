4e5724f0c84b4210f9d9b1db0e91b8b84f9bc7a2
linjoehan <38896202+linjoehan@users.noreply.github.com>
2018-11-27 02:54:34 +0200

    Add files via upload

---
+++ src/main/java/com/codingame/game/Referee.java
@@ -0,0 +1,547 @@
+package com.codingame.game;
+import com.codingame.gameengine.core.AbstractPlayer.TimeoutException;
+import com.codingame.gameengine.core.AbstractReferee;
+import com.codingame.gameengine.core.MultiplayerGameManager;
+import com.codingame.gameengine.module.entities.Circle;
+import com.codingame.gameengine.module.entities.GraphicEntityModule;
+import com.google.inject.Inject;
+import java.util.ArrayList;
+
+public class Referee extends AbstractReferee {
+    // Uncomment the line below and comment the line under it to create a Solo Game
+    // @Inject private SoloGameManager<Player> gameManager;
+    @Inject private MultiplayerGameManager<Player> gameManager;
+    @Inject private GraphicEntityModule graphicEntityModule;
+    
+    private char[][] board = new char[8][8];
+    private int gameturn;
+    
+    private int PIECE_RADIUS = 35;
+    private int BOARD_X = 582;
+    private int BOARD_Y = 189;
+    private int BOARD_DX = 102;
+    private int BOARD_DY = 102;
+    
+    private Circle[] boardpiece = new Circle[24];
+    
+    private ArrayList<String> getMoves()
+    {
+    	ArrayList<String> final_move_list = new ArrayList<String>();
+    	ArrayList<String> j_move = new ArrayList<String>();
+    	ArrayList<Integer> j_move_status = new ArrayList<Integer>();
+        
+        //find all pieces with potential jumps
+        for(int row = 0;row<8;row++)
+        {
+            for(int col = 0;col<8;col++)
+            {
+            	boolean p1turn = (gameturn%2 == 0);
+                
+                if(
+                (board[row][col] == 'b' && p1turn) ||
+                (board[row][col] == 'B' && p1turn) ||
+                (board[row][col] == 'w' && !p1turn) ||
+                (board[row][col] == 'W' && !p1turn)
+                )
+                {
+                    String square = "";
+                    square = "" + (char)(col + 'a');
+                    square = square + (char)(8 - row + '0');
+                    
+                    j_move.add(square);
+                    j_move_status.add(0);
+                }
+            }
+        }
+        
+        //add to potential jumps that are not complete
+        boolean all_done = false;
+        while(!all_done)
+        {
+        	all_done = true;
+        	for(int i = 0;i<j_move.size();i++)
+        	{
+        		if(j_move_status.get(i) == 0)
+        		{
+        			for(int d = 0;d<4;d++)
+        			{
+        				String newmove = add_jump(j_move.get(i),d);
+        				if(newmove.length() != j_move.get(i).length())
+        				{
+        					j_move_status.set(i, 2); //not a legal move anymore
+        					j_move.add(newmove);
+        					j_move_status.add(0);
+        				}
+        			}
+        			if(j_move_status.get(i) == 0)
+        			{
+        				j_move_status.set(i, 1);
+        			}
+        		}
+        	}
+        	
+        	for(int i = 0;i<j_move.size();i++)
+        	{
+        		if(j_move_status.get(i) == 0)
+        		{
+        			all_done = false;
+        		}
+        	}
+        }
+        
+        //add legal jump moves to final list
+        for(int i = 0; i<j_move.size();i++)
+        {
+        	if(j_move.get(i).length() > 2 && j_move_status.get(i) == 1)
+        	{
+        		final_move_list.add(j_move.get(i));
+        	}
+        }
+        
+        //add normal moves if there are no jumps
+        if(final_move_list.size() == 0)
+        {
+        	for(int row = 0;row<8;row++)
+        	{
+        		for(int col = 0;col<8;col++)
+        		{
+        			boolean p1turn = (gameturn%2 == 0);
+        			
+        			if(
+        	        (board[row][col] == 'b' && p1turn) ||
+        	        (board[row][col] == 'B' && p1turn) ||
+        	        (board[row][col] == 'w' && !p1turn) ||
+        	        (board[row][col] == 'W' && !p1turn)
+        	        )
+        			{
+        				//up right move
+        				if(row > 0 && col < 7 && board[row][col] != 'w')
+        				{
+        					if(board[row-1][col+1] == '.')
+        					{
+        						String move_string = "";
+        						move_string = move_string + (char)(col+'a');
+        						move_string = move_string + (char)(8 - row + '0');
+        						move_string = move_string + (char)((col+1) + 'a');
+        						move_string = move_string + (char)(8 - (row-1) + '0');
+        						final_move_list.add(move_string);
+        					}
+        				}
+        				
+        				//up left move
+        				if(row > 0 && col > 0 && board[row][col] != 'w')
+        				{
+        					if(board[row-1][col-1] == '.')
+        					{
+        						String move_string = "";
+        						move_string = move_string + (char)(col+'a');
+        						move_string = move_string + (char)(8 - row + '0');
+        						move_string = move_string + (char)((col-1) + 'a');
+        						move_string = move_string + (char)(8 - (row-1) + '0');
+        						final_move_list.add(move_string);
+        					}
+        				}
+        				
+        				//down right move
+        				if(row < 7 && col < 7 && board[row][col] != 'b')
+        				{
+        					if(board[row+1][col+1] == '.')
+        					{
+        						String move_string = "";
+        						move_string = move_string + (char)(col+'a');
+        						move_string = move_string + (char)(8 - row + '0');
+        						move_string = move_string + (char)((col+1) + 'a');
+        						move_string = move_string + (char)(8 - (row+1) + '0');
+        						final_move_list.add(move_string);
+        					}
+        				}
+        				
+        				//down left move
+        				if(row < 7 && col > 0 && board[row][col] != 'b')
+        				{
+        					if(board[row+1][col-1] == '.')
+        					{
+        						String move_string = "";
+        						move_string = move_string + (char)(col+'a');
+        						move_string = move_string + (char)(8 - row + '0');
+        						move_string = move_string + (char)((col-1) + 'a');
+        						move_string = move_string + (char)(8 - (row+1) + '0');
+        						final_move_list.add(move_string);
+        					}
+        				}
+        			}
+        		}
+        	}
+        }
+        
+        return final_move_list;
+    }
+    
+    private String add_jump(String inputmove,int dir)
+    {
+    	String move = inputmove;
+    	int first_row = 8 - move.charAt(1) + '0';
+    	int first_col = move.charAt(0) - 'a';
+    	
+    	char piece = board[first_row][first_col];
+    	
+    	//board to mark previous jumps
+    	boolean cap[][] = new boolean[8][8];
+    	for(int row = 0;row<8;row++)
+    	{
+    		for(int col = 0;col<8;col++)
+    		{
+    			cap[row][col] = false;
+    		}
+    	}
+    	
+    	//mark completed captures
+    	int current_row = first_row;
+    	int current_col = first_col;
+    	int string_pos = 2;
+    	while(move.length() > string_pos)
+    	{
+    		int next_row = 8 - move.charAt(string_pos + 1) + '0';
+    		int next_col = move.charAt(string_pos) - 'a';
+    		cap[(current_row + next_row) / 2][(current_col + next_col) / 2] = true;
+    		current_row = next_row;
+            current_col = next_col;
+            string_pos += 2;
+    	}
+    	
+    	//NOTE: CLEAN THIS UP
+    	//add in next capture if possible
+    	if(dir == 0 && current_row > 1 && current_col < 6 && piece != 'w') //up right capture
+    	{
+    		//white captures black
+    		if( (piece == 'w' || piece == 'W') &&
+    		    (board[current_row - 1][current_col+1] == 'b' || board[current_row - 1][current_col+1] == 'B') &&
+    		    (board[current_row-2][current_col+2] == '.') &&
+    		    (cap[current_row-1][current_col+1] == false)
+    		)
+    		{
+    			move = move + (char)((current_col + 2)+'a');
+                move = move + (char)(8 - (current_row - 2) + '0');
+    		}
+    		
+    		//black captures white
+    		if( (piece == 'b' || piece == 'B') &&
+        	    (board[current_row - 1][current_col+1] == 'w' || board[current_row - 1][current_col+1] == 'W') &&
+        	    (board[current_row-2][current_col+2] == '.') &&
+        	    (cap[current_row-1][current_col+1] == false)
+        	)
+        	{
+    			move = move + (char)((current_col + 2)+'a');
+    			move = move + (char)(8 - (current_row - 2) + '0');
+        	}
+    	}
+    	
+    	if(dir == 1 && current_row > 1 && current_col > 1 && piece != 'w') //up left capture
+    	{
+    		//white captures black
+    		if( (piece == 'w' || piece == 'W') &&
+    		    (board[current_row - 1][current_col-1] == 'b' || board[current_row - 1][current_col-1] == 'B') &&
+    		    (board[current_row-2][current_col-2] == '.') &&
+    		    (cap[current_row-1][current_col-1] == false)
+    		)
+    		{
+    			move = move + (char)((current_col - 2)+'a');
+                move = move + (char)(8 - (current_row - 2) + '0');
+    		}
+    		
+    		//black captures white
+    		if( (piece == 'b' || piece == 'B') &&
+        	    (board[current_row - 1][current_col-1] == 'w' || board[current_row - 1][current_col-1] == 'W') &&
+        	    (board[current_row-2][current_col-2] == '.') &&
+        	    (cap[current_row-1][current_col-1] == false)
+        	)
+        	{
+    			move = move + (char)((current_col - 2)+'a');
+    			move = move + (char)(8 - (current_row - 2) + '0');
+        	}
+    	}
+    	
+    	if(dir == 2 && current_row < 6 && current_col > 1 && piece != 'b') //down left capture
+    	{
+    		//white captures black
+    		if( (piece == 'w' || piece == 'W') &&
+    		    (board[current_row + 1][current_col-1] == 'b' || board[current_row + 1][current_col-1] == 'B') &&
+    		    (board[current_row+2][current_col-2] == '.') &&
+    		    (cap[current_row+1][current_col-1] == false)
+    		)
+    		{
+    			move = move + (char)((current_col - 2)+'a');
+                move = move + (char)(8 - (current_row + 2) + '0');
+    		}
+    		
+    		//black captures white
+    		if( (piece == 'b' || piece == 'B') &&
+        	    (board[current_row + 1][current_col-1] == 'w' || board[current_row + 1][current_col-1] == 'W') &&
+        	    (board[current_row+2][current_col-2] == '.') &&
+        	    (cap[current_row+1][current_col-1] == false)
+        	)
+        	{
+    			move = move + (char)((current_col - 2)+'a');
+    			move = move + (char)(8 - (current_row + 2) + '0');
+        	}
+    	}
+    	
+    	if(dir == 3 && current_row < 6 && current_col < 6 && piece != 'b') //down right capture
+    	{
+    		//white captures black
+    		if( (piece == 'w' || piece == 'W') &&
+    		    (board[current_row + 1][current_col+1] == 'b' || board[current_row + 1][current_col+1] == 'B') &&
+    		    (board[current_row+2][current_col+2] == '.') &&
+    		    (cap[current_row+1][current_col+1] == false)
+    		)
+    		{
+    			move = move + (char)((current_col + 2)+'a');
+                move = move + (char)(8 - (current_row + 2) + '0');
+    		}
+    		
+    		//black captures white
+    		if( (piece == 'b' || piece == 'B') &&
+        	    (board[current_row + 1][current_col+1] == 'w' || board[current_row + 1][current_col+1] == 'W') &&
+        	    (board[current_row+2][current_col+2] == '.') &&
+        	    (cap[current_row+1][current_col+1] == false)
+        	)
+        	{
+    			move = move + (char)((current_col + 2)+'a');
+    			move = move + (char)(8 - (current_row + 2) + '0');
+        	}
+    	}
+    	
+    	return move;
+    }
+    
+    private void sendPlayerInputs(Player player) 
+    {
+        for(int row = 0;row<8;row++)
+        {
+            String str = "";
+            for(int col = 0;col<8;col++)
+            {
+                str = str + board[row][col];
+            }
+            player.sendInputLine(str);
+        }
+        
+        if(gameturn%2==0)
+        {
+        	player.sendInputLine("b");
+        }
+        else
+        {
+        	player.sendInputLine("w");
+        }
+        
+        ArrayList<String> possible_moves = getMoves();
+        int N = possible_moves.size();
+        
+        player.sendInputLine(String.valueOf(N));
+        
+        for(int i = 0;i<N;i++)
+        {
+        	player.sendInputLine(possible_moves.get(i));
+        }
+    }
+    
+    private void make_move(String move) //makes move on board without checking
+    {
+    	//convert string into coordinates
+    	int r = 8 - (move.charAt(1) - '0');
+    	int c = move.charAt(0) - 'a';
+    	//pick up piece
+    	char piece = board[r][c];
+    	board[r][c] = '.';
+    	
+    	//find index of piece
+    	int piece_index = -1;
+    	for(int i = 0;i<24;i++)
+    	{
+    		if(boardpiece[i].getX() == BOARD_X + c*BOARD_DX && boardpiece[i].getY() == BOARD_Y + r*BOARD_DY)
+    		{
+    			piece_index = i;
+    		}
+    	}
+    	
+    	int string_pos = 2;
+    	while(move.length() > string_pos)
+    	{
+    		//translate next coordinate
+    		int next_r = 8 - (move.charAt(string_pos + 1) - '0');
+    		int next_c = move.charAt(string_pos) - 'a';
+    		
+    		//move boardpiece
+    		boardpiece[piece_index]
+    				.setX(BOARD_X + next_c*BOARD_DX)
+    				.setY(BOARD_Y + next_r*BOARD_DY);
+    		
+    		//remove piece from the middle of the squares (If jumping or just the any of the original if not)
+    		board[(r+next_r)/2][(c+next_c)/2] = '.';
+    		
+    		//check if piece must be removed
+    		if( (r+next_r)/2 != r && (r+next_r)/2 != next_r)
+    		{
+    			int remove_piece_index = -1;
+    			for(int i = 0;i<24;i++)
+    			{
+    				if(boardpiece[i].getX() == BOARD_X + ((c+next_c)/2)*BOARD_DX && boardpiece[i].getY() == BOARD_Y + ((r+next_r)/2)*BOARD_DY)
+    	    		{
+    					remove_piece_index = i;
+    	    		}
+    			}
+    			
+    			//remove piece
+    			boardpiece[remove_piece_index]
+    					.setX(2000)
+        				.setY(556);
+    		}
+    		
+            //update new r
+            r = next_r;
+            c = next_c;
+            
+            //change to king if I can
+    		if(piece == 'b' && next_r == 0)
+            {
+                piece = 'B';
+                boardpiece[piece_index]
+                		.setLineWidth(5)
+                		.setLineColor(0xffff00);
+            }
+            if(piece == 'w' && next_r == 7)
+            {
+                piece = 'W';
+                boardpiece[piece_index]
+                		.setLineWidth(5)
+                		.setLineColor(0xffff00);
+            }
+            
+            string_pos = string_pos + 2;
+    	}
+    	
+    	//put down piece on board
+        board[r][c] = piece;
+    }
+    
+    @Override
+    public void init() 
+    {
+    	
+    	//background
+        graphicEntityModule.createSprite().setImage("background.jpg").setAnchor(0);
+        
+        int next_piece_index = 0;
+        
+        //fill board with starting locations
+        for(int row = 0;row<8;row++)
+        {
+            for(int col = 0;col<8;col++)
+            {
+                if( (row+col) % 2 == 1 && row <= 2)
+                {
+                    board[row][col] = 'w';
+                    boardpiece[next_piece_index] = graphicEntityModule.createCircle()
+                    		.setRadius(PIECE_RADIUS)
+                    		.setFillColor(0xff3333)
+                    		.setX(BOARD_X + col*BOARD_DX)
+                    		.setY(BOARD_Y + row*BOARD_DY);
+                    next_piece_index++;
+                    		
+                }
+                else if((row+col) % 2 == 1 && row >= 5)
+                {
+                    board[row][col] = 'b';
+                    boardpiece[next_piece_index] = graphicEntityModule.createCircle()
+                    		.setRadius(PIECE_RADIUS)
+                    		.setFillColor(0x222222)
+                    		.setX(BOARD_X + col*BOARD_DX)
+                    		.setY(BOARD_Y + row*BOARD_DY);
+                    next_piece_index++;
+                    
+                }
+                else
+                {
+                    board[row][col] = '.';
+                }
+            }
+        }
+        
+        
+        
+        gameturn = 0;
+        
+        
+        
+    }
+
+    @Override
+    public void gameTurn(int turn)
+    {
+        // Code your game logic.
+        // See README.md if you want some code to bootstrap your project.
+        
+    	Player player = gameManager.getPlayer(gameturn % 2);
+        //send player inputs
+        sendPlayerInputs(player);
+        player.execute();
+        
+        
+        try 
+        {
+        	//get input from player
+        	String player_move = player.getOutputs().get(0);
+        	
+        	//check that move is legal
+        	ArrayList<String> possible_moves = getMoves();
+        	boolean is_legal_move = false;
+        	for(int i = 0;i<possible_moves.size();i++)
+        	{
+        		if(player_move.equals(possible_moves.get(i)))
+        		{
+        			is_legal_move = true;
+        		}
+        	}
+        	
+        	if(is_legal_move)
+        	{
+        		//apply move to board
+        		make_move(player_move);
+        		
+        		//increment gameturn
+                gameturn++;
+                
+        		//check winner(move list is empty then plyer loses)
+                ArrayList<String> next_turn_moves = getMoves();
+                if(next_turn_moves.size() == 0)
+                {
+                	Player next_player = gameManager.getPlayer(gameturn % 2);
+                	next_player.deactivate("No moves left for next player");
+                	gameManager.endGame();
+                }
+                
+        	}
+        	else
+        	{
+        		player.deactivate("Move:" + player_move + " is not a legal move");
+        		gameManager.endGame();
+        	}
+        	
+        }
+        catch (TimeoutException e)
+        {
+        	player.deactivate("Time limit exceeded!");
+        	gameManager.endGame();
+        }
+        
+    }
+    
+    @Override
+    public void onEnd() 
+    {
+        for (Player p : gameManager.getPlayers()) {
+            p.setScore(p.isActive() ? 1 : 0);
+        }
+    }
+}
---
+++ src/test/java/Agent1.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Agent1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) 
+        {
+            for(int i = 0;i<8;i++)
+            {
+                String input = scanner.nextLine();
+            }
+            String my_player = scanner.nextLine();
+            
+            int N = scanner.nextInt();
+            scanner.nextLine();
+            
+            ArrayList<String> Moves = new ArrayList<String>();
+            for(int i = 0;i<N;i++)
+            {
+            	Moves.add(scanner.nextLine());
+            }
+            
+            System.out.println(Moves.get(0));
+        }
+    }
+}
